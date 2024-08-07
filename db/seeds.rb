4b4cf09662734f7219d228c8284283632388edd0
Linda Badji <linda.badji33@gmail.com>
2024-08-07 18:08:04 +0200

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,38 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+Restaurant.create!(
+  [
+    {
+      name: 'Le Gourmet',
+      address: '123 Rue Principale, Bordeaux',
+      phone_number: '05 23 45 67 89',
+      category: 'french'
+    },
+    {
+      name: 'Pasta Bella',
+      address: '456 Rue Dusecond, Bordeaux',
+      phone_number: '04 56 78 90 12',
+      category: 'italian'
+    },
+    {
+      name: 'Sushi World',
+      address: '789 Rue Dutroisieme, Bordeaux',
+      phone_number: '09 87 65 43 21',
+      category: 'japanese'
+    },
+    {
+      name: 'Taco Fiesta',
+      address: '321 Taco Avenue, Bordeaux',
+      phone_number: '05 43 21 09 87',
+      category: 'belgian'
+    },
+    {
+      name: 'Curry Palace',
+      address: '654 Spice Avenu, Bordeaux',
+      phone_number: '03 21 43 65 87',
+      category: 'chinese'
+    }
+  ]
+)
